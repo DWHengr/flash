cecd84c8f5b2199d27c102c58be93168755c81e6
henamdu <duweihengmr@163.com>
2022-12-21 18:44:22 +0800

    feat: modify login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,24 @@
 import "./index.css";
+import { useState } from "react";
 export default function Login() {
+  const [userinfo, setUserinfo] = useState({ username: "", password: "" });
+  const [loginMsg, setLoginMsg] = useState("");
+
+  const userinfoHandleChange = (event) => {
+    const { name, value } = event.target;
+    setUserinfo({ ...userinfo, [name]: value });
+  };
+
+  const onLogin = () => {
+    if(!userinfo.username){
+      setLoginMsg("用户名不能为空")
+      return
+    }
+    if(!userinfo.password){
+      setLoginMsg("密码不能为空")
+      return
+    }
+  };
   return (
     <div>
       <label
@@ -7,22 +26,38 @@ export default function Login() {
       >
         登录
       </label>
-      <div className="login_box">
-        <input type="text" v-model="username" required />
+      <div className="custom-input-box">
+        <input
+          type="text"
+          name="username"
+          value={userinfo.username}
+          onChange={userinfoHandleChange}
+          required
+        />
         <label>用户名</label>
       </div>
-      <div className="login_box">
-        <input type="text" v-model="username" required />
+      <div className="custom-input-box">
+        <input
+          type="text"
+          name="password"
+          value={userinfo.password}
+          onChange={userinfoHandleChange}
+          required
+        />
         <label>密码</label>
       </div>
       <button
         style={{
           marginTop: 5,
         }}
+        onClick={onLogin}
         className="minor-button"
       >
         登录 Flash
       </button>
+      <div style={{color:'red',marginTop:5}}>
+        {loginMsg}
+      </div>
     </div>
   );
 }
