36074827221807bab334c36e15b0cf999734d5c7
henamdu <duweihengmr@163.com>
2022-12-22 16:24:57 +0800

    feat: add register page

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,3 +1,84 @@
+import { useState } from "react";
 export default function Register() {
-  return <div>注册页面</div>;
+  const [userinfo, setUserinfo] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [registerMsg, setRegisterMsg] = useState("");
+
+  const userinfoHandleChange = (event) => {
+    const { name, value } = event.target;
+    setUserinfo({ ...userinfo, [name]: value });
+  };
+
+  const onRegister = () => {
+    if (!userinfo.username) {
+      setRegisterMsg("用户名不能为空");
+      return;
+    }
+    if (!userinfo.password) {
+      setRegisterMsg("密码不能为空");
+      return;
+    }
+    if (!userinfo.confirmPassword) {
+      setRegisterMsg("确认密码不能为空");
+      return;
+    }
+    if (userinfo.confirmPassword != userinfo.password) {
+      setRegisterMsg("密码不一致");
+      return;
+    }
+    setRegisterMsg("");
+  };
+
+  return (
+    <div>
+      <label
+        style={{ fontSize: 20, color: "rgb(155, 155, 155)", marginBottom: 10 }}
+      >
+        注册
+      </label>
+      <div className="custom-input-box">
+        <input
+          type="text"
+          name="username"
+          value={userinfo.username}
+          onChange={userinfoHandleChange}
+          required
+        />
+        <label>用户名</label>
+      </div>
+      <div className="custom-input-box">
+        <input
+          type="password"
+          name="password"
+          value={userinfo.password}
+          onChange={userinfoHandleChange}
+          required
+        />
+        <label>密码</label>
+      </div>
+      <div className="custom-input-box">
+        <input
+          type="password"
+          name="confirmPassword"
+          value={userinfo.confirmPassword}
+          onChange={userinfoHandleChange}
+          required
+        />
+        <label>确认密码</label>
+      </div>
+      <button
+        style={{
+          marginTop: 5,
+        }}
+        onClick={onRegister}
+        className="minor-button"
+      >
+        注册 Flash
+      </button>
+      <div style={{ color: "red", marginTop: 5 }}>{registerMsg}</div>
+    </div>
+  );
 }
