20aba103178b7ffbf9d2a646965294d315923689
henamdu <duweihengmr@163.com>
2023-01-10 22:13:31 +0800

    fix: modify loading component

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,5 +1,5 @@
 import "./index.css";
-import { useState, useCallback,createContext,useContext } from "react";
+import { useState, useCallback, createContext, useContext } from "react";
 
 const LoadingBarContext = createContext();
 
@@ -10,28 +10,41 @@ export function useLoading() {
 
 export default function Loading(props) {
   const [isLoading, setIsLoading] = useState(false);
+  const [isShowSuccessMsg, setIsShowSuccessMsg] = useState(false);
 
   const showLoading = useCallback(() => {
+    setIsShowSuccessMsg(false);
     setIsLoading(true);
   }, []);
 
   const hideLoading = useCallback(() => {
-    setIsLoading(false);
+    setIsShowSuccessMsg(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1200);
   }, []);
 
   return (
-    <LoadingBarContext.Provider
-      value={{ isLoading, showLoading, hideLoading }}
-    >
+    <LoadingBarContext.Provider value={{ isLoading, showLoading, hideLoading }}>
       <div>
         {isLoading && (
           <div className="loading-box">
             <div className="loading-icon" data-tauri-drag-region>
-              <svg className="loading-icon-content " viewBox="0 0 50 50">
-                <circle className="ring" cx="25" cy="25" r="20"></circle>
-                <circle className="ball" cx="25" cy="5" r="5"></circle>
-              </svg>
-              <div>正在加载中</div>
+              <div className="loading-icon-box">
+                {!isShowSuccessMsg && (
+                  <svg className="loading-icon-content" viewBox="0 0 50 50">
+                    <circle className="ring" cx="25" cy="25" r="20"></circle>
+                    <circle className="ball" cx="25" cy="5" r="5"></circle>
+                  </svg>
+                )}
+                {isShowSuccessMsg && (
+                  <img className="loading-icon-box" src="/finish.svg" />
+                )}
+              </div>
+              {!isShowSuccessMsg && <div>正在加载中</div>}
+              {isShowSuccessMsg && (
+                <div style={{ color: "#38E274" }}>加载完成</div>
+              )}
             </div>
           </div>
         )}
