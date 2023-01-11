f47220c340b2ca774ddde11c35b01132dc06fb22
henamdu <duweihengmr@163.com>
2023-01-11 22:25:46 +0800

    feat: add option create content

--- src/pages/PersonalSetting/Option/index.jsx
+++ src/pages/PersonalSetting/Option/index.jsx
@@ -1,7 +1,17 @@
-export default function Option(){
+import "./index.css";
+import "./iconfont.css";
+export default function Option() {
   return (
-    <div>
-      选项操作页面
+    <div style={{ position: "relative" }}>
+      <div style={{ height: 200, background: "rgba(27, 27, 27, 0.9)" }}>ww</div>
+      <div className="option-add-bar">
+        <div className="option-add-bar-button">
+          <i
+            style={{ fontSize: 30 }}
+            className="option-add-bar-button-icon iconfont icon-zhankai"
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
