fc1a45360c3863171872b8cd339f7f0e90800539
henamdu <duweihengmr@163.com>
2023-01-12 16:52:27 +0800

    fix: modify option setting page

--- src/pages/PersonalSetting/Option/index.jsx
+++ src/pages/PersonalSetting/Option/index.jsx
@@ -1,15 +1,71 @@
 import "./index.css";
 import "./iconfont.css";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 export default function Option() {
+  const [isCreateOption, setIsCreateOption] = useState(false);
+  const optionData = useSelector((state) => state.optionData);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   return (
-    <div style={{ position: "relative" }}>
-      <div style={{ height: 200, background: "rgba(27, 27, 27, 0.9)" }}>ww</div>
-      <div className="option-add-bar">
-        <div className="option-add-bar-button">
-          <i
-            style={{ fontSize: 30 }}
-            className="option-add-bar-button-icon iconfont icon-zhankai"
-          />
+    <div style={{ height: 350 }}>
+      <div className="create-box">
+        {isCreateOption && (
+          <div style={{ height: 140, background: "rgba(27, 27, 27, 0.9)" }}>
+            新增Option
+          </div>
+        )}
+        <div className="option-add-bar">
+          <div className="option-add-bar-button">
+            {!isCreateOption && (
+              <i
+                onClick={() => {
+                  setIsCreateOption(true);
+                }}
+                style={{ fontSize: 30 }}
+                className="option-add-bar-button-icon iconfont icon-zhankai"
+              />
+            )}
+            {isCreateOption && (
+              <i
+                onClick={() => {
+                  setIsCreateOption(false);
+                }}
+                style={{ fontSize: 30 }}
+                className="option-add-bar-button-icon iconfont icon-shouqi"
+              />
+            )}
+            <div
+              style={{
+                overflowY: "scroll",
+                height: isCreateOption ? 170 : 310,
+              }}
+            >
+              {optionData?.currentDataList?.map((item, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="seek-option"
+                    onClick={() => {
+                      setCurrentIndex(index);
+                    }}
+                    style={{
+                      backgroundColor:
+                        index == currentIndex ? "rgb(78, 78, 78)" : "",
+                    }}
+                  >
+                    <img src={item.icon} className="seek-option-icon" />
+                    <div style={{ display: "inline-block" }}>
+                      <div className="seek-option-name">{item.name}</div>
+                      <div className="seek-option-describe">
+                        {item.describe}
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </div>
