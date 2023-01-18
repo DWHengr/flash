ab3b32b6699ef63ccaf256df8a34e49296199fda
henamdu <duweihengmr@163.com>
2023-01-18 10:41:23 +0800

    fix: modify option icon generate

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import PersonalSetting from "./pages/PersonalSetting";
 import Option from "./pages/Option";
 import { useSelector, useDispatch } from "react-redux";
 import { Route, Switch, useHistory } from "react-router-dom";
+import { setOptionIcon } from "./utils/flash";
 
 function App() {
   const [content, setContent] = useState("");
@@ -42,37 +43,7 @@ function App() {
     const client = await getClient();
     for (let index = 0; index < options?.length; index++) {
       let o = options[index];
-      o.icon = "/icon.svg";
-
-      if (o.option_type == "project") o.icon = "/project.svg";
-      if (o.option_type == "file") o.icon = "/file.svg";
-      if (o.option_type == "app") o.icon = "/app.svg";
-      if (o.option_type == "folder") o.icon = "/folder.svg";
-      if (o.option_type == "link") {
-        o.icon = "/link.svg";
-        try {
-          let pattern = new RegExp("^https?://[^/]*", "i");
-          let domain = pattern.exec(o.path);
-          if (domain)
-            client
-              .get(domain[0] + "/favicon.ico", {
-                timeout: 5,
-                responseType: ResponseType.Binary,
-              })
-              .then((res) => {
-                if (res?.status == 200) {
-                  o.icon =
-                    "data:image/png;base64," +
-                    btoa(
-                      new Uint8Array(res.data).reduce(
-                        (data, byte) => data + String.fromCharCode(byte),
-                        ""
-                      )
-                    );
-                }
-              });
-        } catch {}
-      }
+      setOptionIcon(o);
     }
   };
 
