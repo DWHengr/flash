57bc0ced0680da4dd870404a855db67e8a0cd37f
henamdu <duweihengmr@163.com>
2023-02-07 19:24:34 +0800

    fix: modify login tip info

--- src/pages/PersonalSetting/Login/index.jsx
+++ src/pages/PersonalSetting/Login/index.jsx
@@ -8,6 +8,7 @@ import { useHistory } from "react-router";
 export default function Login() {
   const [userinfo, setUserinfo] = useState({ username: "", password: "" });
   const [loginMsg, setLoginMsg] = useState("");
+  const [isLogging, setIsLogging] = useState(false);
   const h = useHistory();
 
   const userinfoHandleChange = (event) => {
@@ -24,13 +25,24 @@ export default function Login() {
       setLoginMsg("密码不能为空");
       return;
     }
+    setIsLogging(true);
     setLoginMsg("");
-    user.login(userinfo).then((res) => {
-      if (res.code == 0) {
-        h.push("/personal/option");
-        dispatch(setUser(res.data.token, res.data.username));
-      }
-    });
+    user
+      .login(userinfo)
+      .then((res) => {
+        if (res.code == 0) {
+          h.push("/personal/option");
+          dispatch(setUser(res.data.token, res.data.username));
+        } else {
+          setLoginMsg(res.msg);
+        }
+      })
+      .catch((res) => {
+        setLoginMsg(res.message);
+      })
+      .finally(() => {
+        setIsLogging(false);
+      });
   };
   return (
     <div className={"login-register-box"}>
@@ -62,8 +74,9 @@ export default function Login() {
         }}
         onClick={onLogin}
         className="minor-button"
+        disabled={isLogging}
       >
-        登录 Flash
+        {isLogging ? "登录中..." : "登录 Flash"}
       </button>
       <div style={{ color: "#FCDA01", marginTop: 5 }}>{loginMsg}</div>
     </div>
