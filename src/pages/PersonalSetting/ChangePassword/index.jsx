b1c5e0c54c1c1b0cc7bdfb5560c94601664d4940
henamdu <duweihengmr@163.com>
2023-03-15 17:15:31 +0800

    fix: modify ChangePassword page

--- src/pages/PersonalSetting/ChangePassword/index.jsx
+++ src/pages/PersonalSetting/ChangePassword/index.jsx
@@ -1,14 +1,59 @@
 import "./index.css";
 import { useState } from "react";
+import FlahsInput from "../../../components/FlashInput";
 export default function ChangePassword() {
+
+  const [pwdinfo, setPwdinfo] = useState({
+    oldPassword: "",
+    newPassword: "",
+    confirmPassword: "",
+  });
+
+  const pwdinfoHandleChange = (event) => {
+    const { name, value } = event.target;
+    setPwdinfo({ ...userinfo, [name]: value });
+  };
 
   return (
     <div>
       <div className="option-bar">
         <div>修改密码</div>
       </div>
-      <div className={"center-box"}>
-
+      <div className={"change-pwd-box"}>
+      <FlahsInput
+          width={260}
+          type="text"
+          name="oldPassword"
+          vlaue={pwdinfo.oldPassword}
+          onChange={pwdinfoHandleChange}
+        >
+          原密码
+        </FlahsInput>
+        <FlahsInput
+          width={260}
+          type="text"
+          name="newPassword"
+          vlaue={pwdinfo.newPassword}
+          onChange={pwdinfoHandleChange}
+        >
+          新密码
+        </FlahsInput>
+        <FlahsInput
+          width={260}
+          type="text"
+          name="confirmPassword"
+          vlaue={pwdinfo.newPassword}
+          onChange={pwdinfoHandleChange}
+        >
+          确认密码
+        </FlahsInput>
+        <div>
+        <div className="change-pwd-option">
+        <button style={{width:"100px"}} className="minor-button">
+                确认修改
+              </button>
+          </div>
+        </div>
       </div>
     </div>
   );
