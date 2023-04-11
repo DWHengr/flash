cf8821858055ec3ec625e6827080fc23ced276c9
Heath <duweihengmr@163.com>
2023-04-11 19:40:47 +0800

    fix: modify variable name page

---
+++ src/pages/VariableName/index.jsx
@@ -0,0 +1,56 @@
+import "./index.css";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { setCurrentListLenghtStore } from "../../store/search/action";
+import translate from "../../api/translate";
+
+export default function VariableName() {
+  const searchData = useSelector((state) => state.searchData);
+  const [currentDataList, setCurrentDataList] = useState([]);
+  const dispatch = useDispatch();
+  const onOptionClick = (index) => {};
+
+  useEffect(() => {
+    console.log(searchData.trigger);
+    if (!searchData.trigger) return;
+    if (!searchData.content) return;
+    translate
+      .variableNames({ variableName: searchData.searchValue })
+      .then((res) => {
+        if (res.code == 0) {
+          let currentOption = res.data;
+          console.log(currentOption);
+          dispatch(setCurrentListLenghtStore(currentOption.length));
+          setCurrentDataList(currentOption);
+        }
+      });
+  }, [searchData.trigger]);
+
+  return (
+    <div>
+      {currentDataList?.length <= 0 && (
+        <div className="variable-bar">暂时无数据 Enter获取变量命名</div>
+      )}
+      {currentDataList?.map((item, index) => {
+        return (
+          <div
+            key={item.name}
+            className="seek-option"
+            onClick={() => onOptionClick(index)}
+            style={{
+              backgroundColor:
+                index == searchData.currentIndex ? "rgb(78, 78, 78)" : "",
+            }}
+          >
+            <img src={`/${item.name}.svg`} className="seek-option-icon" />
+
+            <div style={{ display: "inline-block" }}>
+              <div className="seek-option-name">{item.value}</div>
+              <div className="seek-option-describe">{item.describe}</div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
