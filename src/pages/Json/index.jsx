4bf4699e442d337cc4e35a528c0d682e3cd0faf8
Heath <duweihengmr@163.com>
2023-04-19 19:19:53 +0800

    feat: modify json tool page

--- src/pages/Json/index.jsx
+++ src/pages/Json/index.jsx
@@ -1,8 +1,23 @@
 import "./index.css";
+import { useSelector } from "react-redux";
+import { useState } from "react";
 export default function Json() {
+  const settingData = useSelector((state) => state.settingData);
+  let [jsonData, setJsonData] = useState({
+    name: "flash",
+    data: ["1", "2"],
+  });
   return (
-    <div>
-      <div>Json工具页面</div>
+    <div
+      className="json-box"
+      style={{ height: settingData.windowHeight - settingData.searchBoxHeight }}
+    >
+      <div className="json-from-box">{JSON.stringify(jsonData,null, 2)}</div>
+      <div className="json-operate-box">
+        <div className="json-button">格式化</div>
+        <div className="json-button">压缩</div>
+      </div>
+      <div className="json-to-box"></div>
     </div>
   );
 }
