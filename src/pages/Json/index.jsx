82af2bad695f8917d4ac20fddcc868e24445c347
Heath <duweihengmr@163.com>
2023-04-20 21:43:49 +0800

    feat: use react-json-view package

--- src/pages/Json/index.jsx
+++ src/pages/Json/index.jsx
@@ -1,23 +1,78 @@
 import "./index.css";
 import { useSelector } from "react-redux";
 import { useState } from "react";
+import ReactJson from "react-json-view";
 export default function Json() {
   const settingData = useSelector((state) => state.settingData);
-  let [jsonData, setJsonData] = useState({
-    name: "flash",
-    data: ["1", "2"],
-  });
+
+  let [model, setModel] = useState("");
+  let [jsonFromData, setJsonFromData] = useState("");
+  let [jsonToData, setJsonToData] = useState({});
+  let onFormat = () => {
+    setModel("format");
+    let data = JSON.parse(jsonFromData);
+    setJsonToData(data);
+  };
+
+  let onCompress = () => {
+    setModel("compress");
+    let data = JSON.parse(jsonFromData);
+    data = JSON.stringify(data);
+    setJsonToData(data);
+  };
+
+  let jsonTheme = {
+    base00: "",
+    base01: "",
+    base02: "",
+    base03: "",
+    base04: "purple",
+    base05: "",
+    base06: "",
+    base07: "",
+    base08: "",
+    base09: "",
+    base0A: "",
+    base0B: "",
+    base0C: "",
+    base0D: "",
+    base0E: "rgba(70, 70, 230, 1)",
+    base0F: "",
+  };
   return (
     <div
       className="json-box"
       style={{ height: settingData.windowHeight - settingData.searchBoxHeight }}
     >
-      <div className="json-from-box">{JSON.stringify(jsonData,null, 2)}</div>
+      <textarea
+        className="json-from-box"
+        value={jsonFromData}
+        onChange={(e) => {
+          setJsonFromData(e.target.value);
+        }}
+      ></textarea>
       <div className="json-operate-box">
-        <div className="json-button">格式化</div>
-        <div className="json-button">压缩</div>
+        <div className="json-button" onClick={onFormat}>
+          格式化
+        </div>
+        <div className="json-button" onClick={onCompress}>
+          压缩
+        </div>
       </div>
-      <div className="json-to-box"></div>
+      <div className="json-to-box">
+        {model == "format" && (
+          <ReactJson
+            style={{ fontFamily: "sans-serif" }}
+            name={false}
+            displayDataTypes={false}
+            src={jsonToData}
+            theme={jsonTheme}
+          ></ReactJson>
+        )}
+        {model == "compress" && (
+          <div style={{ wordWrap: "break-word" }}>{jsonFromData}</div>
+        )}
+      </div>
     </div>
   );
 }
