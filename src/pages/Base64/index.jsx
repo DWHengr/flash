222c243b72fd34cf3f8af6c4db2b9a85a0ead61f
Heath <duweihengmr@163.com>
2023-04-25 20:58:56 +0800

    fix: modify base64 tool page

--- src/pages/Base64/index.jsx
+++ src/pages/Base64/index.jsx
@@ -1,9 +1,21 @@
 import "./index.css";
 import { useSelector } from "react-redux";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { encode, decode } from "../../utils/base64";
 export default function Base64() {
   const settingData = useSelector((state) => state.settingData);
+  const searchData = useSelector((state) => state.searchData);
+
   const [content, setContent] = useState("");
+  const [resultContent, setResultContent] = useState("");
+
+  useEffect(() => {
+    if (!searchData.trigger.enter) return;
+    if (!searchData.content) return;
+    setContent(searchData.searchValue);
+    setResultContent(encode(searchData.searchValue));
+  }, [searchData.trigger]);
+
   return (
     <div
       className="base64-box"
@@ -18,16 +30,26 @@ export default function Base64() {
       ></textarea>
       <div className="base64-bar">
         <div style={{ position: "relative" }}>
-          <div className="base64-button" onClick={() => {}}>
+          <div
+            className="base64-button"
+            onClick={() => {
+              setResultContent(encode(content));
+            }}
+          >
             base64加密
           </div>
-          <div className="base64-button" onClick={() => {}}>
+          <div
+            className="base64-button"
+            onClick={() => {
+              setResultContent(decode(content));
+            }}
+          >
             base64解码
           </div>
           <div className="base64-from-to"></div>
         </div>
       </div>
-      <div className="base64-to-box">{}</div>
+      <div className="base64-to-box">{resultContent}</div>
     </div>
   );
 }
