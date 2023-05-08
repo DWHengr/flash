f5699d32001e382a419f7783597dede4ff1c59cd
Heath <duweihengmr@163.com>
2023-05-08 17:33:09 +0800

    fix: modify default page

--- src/pages/Default/index.jsx
+++ src/pages/Default/index.jsx
@@ -1,40 +1,70 @@
 import "./index.css";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { useState } from "react";
-import { setCurrentListLenghtStore } from "../../store/search/action";
+import { useState, useEffect } from "react";
+import {
+  setContentStore,
+  setCurrentListLenghtStore,
+} from "../../store/search/action";
 import { openUrl } from "../../utils/command";
+import { optionPageRoutes } from "../pageRoutes";
 
 export default function Default() {
   const searchData = useSelector((state) => state.searchData);
   const settingData = useSelector((state) => state.settingData);
+  const [currentDataList, setCurrentDataList] = useState([]);
   const allEngine = [
     { name: "baidu", icon: "/baidu.ico" },
     { name: "biying", icon: "/biying.ico" },
     { name: "csdn", icon: "/csdn.ico" },
   ];
-  const [currentEngine, setCurrentEngine] = useState([]);
+  const optionRoutes = optionPageRoutes;
   const dispatch = useDispatch();
-  const onOptionClick = (index) => {
-    openSearchEngine(currentEngine[index].name, searchData.content);
-  };
   useEffect(() => {
     if (!searchData.content) return;
-    let newCurrentEngine = [];
-    newCurrentEngine = allEngine.filter(
-      (item) => item.name?.indexOf(settingData.search_engine) != -1
-    );
-    setCurrentEngine(newCurrentEngine);
-    dispatch(setCurrentListLenghtStore(newCurrentEngine.length));
+    let currentData = [];
+    allEngine.map((item) => {
+      if (item.name?.indexOf(settingData.search_engine) != -1) {
+        currentData.push({
+          name: `${item.name} 搜索`,
+          icon: item.icon,
+          describe: "默认搜索引擎",
+        });
+      }
+    });
+
+    optionRoutes.map((item) => {
+      item.cmds?.map((cmd) => {
+        console.log(cmd.indexOf(searchData.content));
+        if (cmd.indexOf(searchData.content) != -1) {
+          currentData.push({
+            name: cmd,
+            icon: item.icon,
+            describe: item.describe,
+            path: item.path,
+          });
+        }
+      });
+    });
+    setCurrentDataList(currentData);
+    dispatch(setCurrentListLenghtStore(currentData.length));
   }, [searchData.content]);
 
   useEffect(() => {
     if (!searchData.trigger.enter) return;
-    openSearchEngine(
-      currentEngine[searchData.currentIndex].name,
-      searchData.content
-    );
+    openOption(currentDataList[searchData.currentIndex], searchData.content);
   }, [searchData.trigger]);
+
+  const onOptionClick = (index) => {
+    openOption(currentDataList[index], searchData.content);
+  };
+
+  const openOption = (option, content) => {
+    if (option.path) {
+      dispatch(setContentStore(`${option.name}:`));
+    } else {
+      openSearchEngine(option.name, content);
+    }
+  };
 
   const openSearchEngine = (engine, value) => {
     switch (engine) {
@@ -52,7 +82,7 @@ export default function Default() {
   };
   return (
     <div>
-      {currentEngine?.map((item, index) => {
+      {currentDataList?.map((item, index) => {
         return (
           <div
             key={index}
@@ -65,8 +95,16 @@ export default function Default() {
           >
             <img src={item.icon} className="seek-option-icon" />
             <div style={{ display: "inline-block" }}>
-              <div className="seek-option-name">{item.name} 搜索</div>
-              <div className="seek-option-describe">默认搜索引擎</div>
+              <div
+                className="seek-option-name"
+                dangerouslySetInnerHTML={{
+                  __html: item.name?.replace(
+                    searchData.content,
+                    `<span class="seek-option-keyword">${searchData.content}</span>`
+                  ),
+                }}
+              ></div>
+              <div className="seek-option-describe">{item.describe}</div>
             </div>
           </div>
         );
