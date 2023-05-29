4999bd6b4ec081bf16291fe3b75b17d0f953496f
Heath <duweihengmr@163.com>
2023-05-29 21:19:15 +0800

    fix: modify schedule page

--- src/pages/Schedule/index.jsx
+++ src/pages/Schedule/index.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 export default function Schedule() {
   const settingData = useSelector((state) => state.settingData);
-  const [isAdd, setIsAdd] = useState(true);
+  const [isAdd, setIsAdd] = useState(false);
 
   return (
     <div
@@ -88,8 +88,35 @@ export default function Schedule() {
           </tbody>
         </table>
       </div>
-      <div className="schedule-add-btn" onClick={()=>{setIsAdd(!isAdd)}}>添加日程</div>
-      {isAdd && <div className="schedule-add"></div>}
+      <div
+        className="schedule-add-btn"
+        onClick={() => {
+          setIsAdd(!isAdd);
+        }}
+      >
+        添加日程
+      </div>
+      {isAdd && (
+        <div className="schedule-add-box ">
+          <div>
+            <div>开始时间:</div>
+            <input type="datetime-local" class="text-input "></input>
+          </div>
+          <div>
+            <div>结束时间:</div>
+            <input type="datetime-local" class="text-input "></input>
+          </div>
+          <div>
+            <div>结束时间:</div>
+            <textarea
+              style={{ resize: "none", height: 120 }}
+              type="text"
+              class="text-input "
+              placeholder="请输入日程内容"
+            ></textarea>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
