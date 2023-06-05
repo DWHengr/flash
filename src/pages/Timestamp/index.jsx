1a7dbf0d72a3ccd390b1aac8807260fe6c5e981d
Heath <duweihengmr@163.com>
2023-06-05 19:19:07 +0800

    fix: modify timestamp page

--- src/pages/Timestamp/index.jsx
+++ src/pages/Timestamp/index.jsx
@@ -1,8 +1,20 @@
 import "./index.css";
+import React, { useState, useEffect } from "react";
 export function Timestamp() {
+  const [timestamp, setTimestamp] = useState(Date.now());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimestamp(Date.now());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
     <div>
-      <div>时间戳工具</div>
+      <div>当前时间戳：{timestamp}</div>
     </div>
   );
 }
