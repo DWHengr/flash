b8d259a97e21f1f4b3767407d857bd363a63dd06
Heath <duweihengmr@163.com>
2023-06-06 22:46:39 +0800

    fix: modify date util

--- src/utils/date.js
+++ src/utils/date.js
@@ -21,3 +21,14 @@ export function formatDate(dateStr) {
 
   return `${year}-${month}-${day} ${hours}:${minutes}`;
 }
+
+export function formatDateTime(time) {
+  const date = new Date(parseInt(time));
+  const year = date.getFullYear();
+  const month = padZero(date.getMonth() + 1);
+  const day = padZero(date.getDate());
+  const hours = padZero(date.getHours());
+  const minutes = padZero(date.getMinutes());
+  const seconds = padZero(date.getSeconds());
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
