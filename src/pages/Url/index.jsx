09e144420993143f078242c73e677e857054d89a
Heath <duweihengmr@163.com>
2023-06-13 19:55:11 +0800

    fix: modify url page

--- src/pages/Url/index.jsx
+++ src/pages/Url/index.jsx
@@ -1,8 +1,61 @@
 import "./index.css";
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+
 export default function Url() {
+  const settingData = useSelector((state) => state.settingData);
+  const searchData = useSelector((state) => state.searchData);
+
+  const [content, setContent] = useState("");
+  const [resultContent, setResultContent] = useState("");
+
+  useEffect(() => {
+    if (!searchData.trigger.enter) return;
+    if (!searchData.content) return;
+    setContent(searchData.searchValue);
+    setResultContent(encodeURIComponent(searchData.searchValue));
+  }, [searchData.trigger]);
+
   return (
-    <div>
-      <div>url编码/解码</div>
+    <div
+      className="url-box"
+      style={{ height: settingData.windowHeight - settingData.searchBoxHeight }}
+    >
+      <textarea
+        className="url-from-box"
+        value={content}
+        onChange={(e) => {
+          setContent(e.target.value);
+        }}
+      ></textarea>
+      <div className="url-bar">
+        <div style={{ position: "relative" }}>
+          <div
+            className="url-button"
+            onClick={() => {
+              setResultContent(encodeURIComponent(content));
+            }}
+          >
+            url编码
+          </div>
+          <div
+            className="url-button"
+            onClick={() => {
+              setResultContent(decodeURIComponent(content));
+            }}
+          >
+            url解码
+          </div>
+          <div className="url-from-to"></div>
+        </div>
+      </div>
+      <textarea
+        className="url-to-box"
+        value={resultContent}
+        onChange={(e) => {
+          setResultContent(e.target.value);
+        }}
+      ></textarea>
     </div>
   );
 }
