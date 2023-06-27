9bf6d57f96069ad680985cb3a937de304129da89
Heath <duweihengmr@163.com>
2023-06-27 21:54:59 +0800

    fix: modify simplified and traditional page

--- src/pages/Sat/index.jsx
+++ src/pages/Sat/index.jsx
@@ -1,8 +1,62 @@
 import "./index.css";
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import Chinese from "chinese-s2t";
+
 export default function Sat() {
+  const settingData = useSelector((state) => state.settingData);
+  const searchData = useSelector((state) => state.searchData);
+
+  const [content, setContent] = useState("");
+  const [resultContent, setResultContent] = useState("");
+
+  useEffect(() => {
+    if (!searchData.trigger.enter) return;
+    if (!searchData.content) return;
+    setContent(searchData.searchValue);
+    setResultContent(Chinese.s2t(searchData.searchValue));
+  }, [searchData.trigger]);
+
   return (
-    <div>
-      <div>简体/繁体转换</div>
+    <div
+      className="url-box"
+      style={{ height: settingData.windowHeight - settingData.searchBoxHeight }}
+    >
+      <textarea
+        className="url-from-box"
+        value={content}
+        onChange={(e) => {
+          setContent(e.target.value);
+        }}
+      ></textarea>
+      <div className="url-bar">
+        <div style={{ position: "relative" }}>
+          <div
+            className="url-button"
+            onClick={() => {
+              setResultContent(Chinese.s2t(content));
+            }}
+          >
+            繁体
+          </div>
+          <div
+            className="url-button"
+            onClick={() => {
+              setResultContent(Chinese.t2s(content));
+            }}
+          >
+            简体
+          </div>
+          <div className="url-from-to"></div>
+        </div>
+      </div>
+      <textarea
+        className="url-to-box"
+        value={resultContent}
+        onChange={(e) => {
+          setResultContent(e.target.value);
+        }}
+      ></textarea>
     </div>
   );
 }
