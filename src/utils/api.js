535b4b108aa8b5aba9613ed7ebb2115cf73248a2
Heath <duweihengmr@163.com>
2023-07-16 20:18:11 +0800

    fix: modify api util

--- src/utils/api.js
+++ src/utils/api.js
@@ -33,7 +33,37 @@ function send(configs) {
           });
         }
       })
-      .catch(() => reject({ message: "网络错误" }));
+      .catch((e) => reject({ message: `网络错误: ${e}` }));
+  });
+}
+
+function send_api(configs) {
+  return new Promise(async (resolve, reject) => {
+    const client = await getClient();
+    client
+      .request({
+        url: configs.url,
+        method: configs.method,
+        timeout: 10,
+        body: {
+          type: "Json",
+          payload: configs.data ? configs.data : {},
+        },
+        headers: {
+          ...configs.headers,
+        },
+        responseType: ResponseType.Text,
+      })
+      .then((response) => {
+        if (response.ok) {
+          return resolve(response.data);
+        } else {
+          reject({
+            message: "Request failed with status code " + response.status,
+          });
+        }
+      })
+      .catch((e) => reject({ message: `网络错误: ${e}` }));
   });
 }
 
@@ -66,4 +96,25 @@ function post(url, params = {}) {
   return send(configs);
 }
 
-export default { post, get };
+function get_api(api, url, params = {}) {
+  let urlParams = [];
+  Object.keys(params).forEach((key) => {
+    urlParams.push(`${key}=${encodeURIComponent(params[key])}`);
+  });
+  if (urlParams.length) {
+    urlParams = `${api + url}?${urlParams.join("&")}`;
+  } else {
+    urlParams = api + url;
+  }
+  console.log(urlParams);
+  const configs = {
+    url: urlParams,
+    method: "GET",
+    params: {
+      randomTime: new Date().getTime(),
+    },
+  };
+  return send_api(configs);
+}
+
+export default { post, get, get_api };
