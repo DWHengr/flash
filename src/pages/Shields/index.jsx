4750d9471447bb7b6e1539ad779731f23396bc0d
Heath <duweihengmr@163.com>
2023-07-17 18:49:29 +0800

    fix: modify shields page

--- src/pages/Shields/index.jsx
+++ src/pages/Shields/index.jsx
@@ -1,8 +1,69 @@
+import shields from "../../api/shields";
 import "./index.css";
+import { useState } from "react";
+import { PopoverPicker } from "../../components/PopoverPicker";
 export default function Shields() {
+  let [imgSrc, setImgSrc] = useState("");
+  let [label, setLabel] = useState("label");
+  let [message, setMessage] = useState("message");
+  let [color, setColor] = useState("#4C1");
+  let onCreateShield = async () => {
+    shields
+      .create(
+        encodeURIComponent(
+          `${label ? label : "label"}-${message ? message : "message"}-${
+            color ? color : "#4C1"
+          }`
+        )
+      )
+      .then((res) => {
+        setImgSrc(res);
+      });
+  };
+
   return (
-    <div>
-      <div>shields徽章生成</div>
+    <div className="shd-box">
+      <div style={{ margin: 5 }}>
+        颜色：
+        <input
+          value={color}
+          onChange={(e) => {
+            setColor(e.target.value);
+          }}
+        />
+        <PopoverPicker
+          style={{ display: "inline-block", width: 20, height: 20 }}
+          color={color}
+          onChange={setColor}
+        />
+      </div>
+      <div style={{ margin: 5 }}>
+        文案：
+        <input
+          value={label}
+          onChange={(e) => {
+            setLabel(e.target.value);
+          }}
+        />
+        -
+        <input
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        />
+      </div>
+      <div
+        style={{ margin: 5 }}
+        className="shd-button"
+        onClick={onCreateShield}
+      >
+        徽章生成
+      </div>
+      <div style={{ margin: 5 }}>
+        预览：
+        <div dangerouslySetInnerHTML={{ __html: imgSrc }} />
+      </div>
     </div>
   );
 }
